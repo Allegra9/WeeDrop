98b2dc46781bb373070b750a119a0c3cce10f297
Allegra <aukseallegra@gmail.com>
2018-07-09 13:55:28 +0100

    _form and new

---
+++ app/controllers/buyers_controller.rb
@@ -0,0 +1,27 @@
+class BuyersController < ApplicationController
+
+  def new
+    @buyer = Buyer.new
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def upadte
+
+  end
+
+  def destroy
+
+  end
+
+end
